6d4a065e277ccae524d45fd9a5f4662d05fbbfca
David Schwartz <dbschwartz@protonmail.com>
2017-11-21 16:59:03 -0800

    run webpack

--- src/nep5.js
+++ src/nep5.js
@@ -1,7 +1,8 @@
 import ScriptBuilder from './sc/scriptBuilder.js'
-import { getScriptHashFromAddress } from './wallet'
-import { doInvokeScript, parseVMStack } from './api'
+import { getScriptHashFromAddress, getAccountFromWIFKey } from './wallet'
+import { doInvokeScript, parseVMStack, getBalance, queryRPC } from './api'
 import { reverseHex, fixed82num } from './utils'
+import * as tx from './transactions/index.js'
 
 /**
  * Queries for NEP5 Token information.
@@ -41,4 +42,30 @@ export const getTokenBalance = (net, scriptHash, address) => {
     .then((res) => {
       return fixed82num(res.stack[0].value)
     })
+}
+
+/**
+ * Transfers NEP5 Tokens.
+ * @param {string} net
+ * @param {string} scriptHash
+ * @param {string} fromWif
+ * @param {string} toAddress
+ * @param {number} transferAmount
+ * @param {number} gasCost
+ * @return {Promise<{name: string, symbol: string, decimals: number, totalSupply: number}>}
+ */
+export const doTransferToken = (net, scriptHash, fromWif, toAddress, transferAmount, gasCost = 0) => {
+  const account = getAccountFromWIFKey(fromWif)
+  return getBalance(net, account.address).then((balances) => {
+    const fromAddrScriptHash = reverseHex(getScriptHashFromAddress(account.address))
+    const intents = [
+      { assetId: tx.ASSETS['GAS'], value: 0.1, scriptHash: fromAddrScriptHash }
+    ]
+    const toAddrScriptHash = reverseHex(getScriptHashFromAddress(toAddress))
+    const invoke = { scriptHash, operation: 'transfer', args: [fromAddrScriptHash, toAddrScriptHash, transferAmount] }
+    const unsignedTx = tx.create.invocation(account.publicKeyEncoded, balances, intents, invoke, gasCost, { version: 1 })
+    const signedTx = tx.signTransaction(unsignedTx, account.privateKey)
+    const hexTx = tx.serializeTransaction(signedTx)
+    return queryRPC(net, 'sendrawtransaction', [hexTx], 4)
+  })
 }
--- tests/nep5.js
+++ tests/nep5.js
@@ -1,28 +1,53 @@
-import * as NEP5 from '../src/nep5.js'
+import * as NEP5 from '../src/nep5'
+import { doMintTokens } from '../src/api'
 
-// describe.skip('NEP5', function () {
-//   this.timeout(10000)
-//   it('get basic info', () => {
-//     return NEP5.getTokenInfo('http://localhost:20332', '5b7074e873973a6ed3708862f219a6fbf4d1c411')
-//       .then(result => {
-//         result.name.should.equal('Red Pulse Token 3.1.4')
-//         result.symbol.should.equal('RPX')
-//         result.decimals.should.equal(8)
-//         result.totalSupply.should.be.above(1000)
-//       })
-//       .catch((e) => {
-//         console.log(e)
-//         throw e
-//       })
-//   })
-//   it('get balance', () => {
-//     return NEP5.getTokenBalance('http://localhost:20332', '5b7074e873973a6ed3708862f219a6fbf4d1c411', 'AVf4UGKevVrMR1j3UkPsuoYKSC4ocoAkKx')
-//       .then(result => {
-//         result.should.be.above(0)
-//       })
-//       .catch((e) => {
-//         console.log(e)
-//         throw e
-//       })
-//   })
-// })
+describe('NEP5', function () {
+  const net = 'TestNet'
+  const scriptHash = 'd7678dd97c000be3f33e9362e673101bac4ca654'
+  const mintedAddress = 'ALw4RsYohc9Kp6whudKbSMr44S12XbTXeh'
+  const toAddress = 'ALq7AWrhAueN6mJNqk6FHJjnsEoPRytLdW'
+  const fromWif = 'L5FzBMGSG2d7HVJL5vWuXfxUKsrkX5irFhtw1L5zU4NAvNuXzd8a'
+  const neo = 1
+  const gasCost = 0
+
+  // before(function (done) {
+  //   doMintTokens(net, scriptHash, fromWif, neo, gasCost).then(res => {
+  //     done()
+  //   }).catch(e => done.fail(e))
+  // })
+  this.timeout(10000)
+  it('get basic info', () => {
+    return NEP5.getTokenInfo(net, scriptHash)
+      .then(result => {
+        result.name.should.equal('LOCALTOKEN')
+        result.symbol.should.equal('LWTF')
+        result.decimals.should.equal(8)
+        result.totalSupply.should.equal(1969000)
+      })
+      .catch((e) => {
+        console.log(e)
+        throw e
+      })
+  })
+  it('get balance', () => {
+    return NEP5.getTokenBalance(net, scriptHash, toAddress)
+      .then(result => {
+        result.should.be.above(0)
+      })
+      .catch((e) => {
+        console.log(e)
+        throw e
+      })
+  })
+  // it('transfers tokens', () => {
+  //   const transferAmount = 3
+  //   return NEP5.doTransferToken(net, scriptHash, fromWif, toAddress, transferAmount, gasCost)
+  //     .then(({ result }) => {
+  //       result.should.equal(true)
+  //     })
+  //     .catch((e) => {
+  //       console.log(e)
+  //       throw e
+  //     })
+  // })
+})
